cc45ee7c4a55528033ab190046d40d2ee415c5da
NeverSettels <settels.eric@gmail.com>
2020-02-07 13:49:34 -0800

    Added Css form for entering country and age

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { checkIfNumber, clacMercuryYears, calcVenusYears, calcMarsYears, calcJupiterYears, getLifeExpectancy, leftToLive } from './age.logic.js';
 import $ from 'jquery';
-import './styles.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
