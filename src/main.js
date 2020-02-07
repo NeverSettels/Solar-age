e9eac8b004e8b604111e52267a06e3aaf44d2244
NeverSettels <settels.eric@gmail.com>
2020-02-07 15:51:49 -0800

    Add UI logic for mercurys

--- src/main.js
+++ src/main.js
@@ -16,4 +16,16 @@ $(document).ready(() => {
     $('#age-form').addClass('no-display');
     $('.planets').removeClass('no-display');
   });
+  $('.planets').on('click', 'img', event => {
+    let planet = event.target.alt;
+    $('.planets').addClass('no-display');
+    if (planet === 'mercury') {
+      $('#info').removeClass('no-display')
+      $("#img").attr("src", "https://nineplanets.org/wp-content/uploads/2019/09/mercury.png");
+      $('#title').text(planet);
+      $('#fact').text('Mercury is the closest planet to the Sun and is also the smallest of the eight planets in our solar system. For every 2 orbits of the Sun, which takes around 88 Earth days, Mercury completes three rotations of its axis. It is gravitationally locked and this rotation is unique to the solar system.');
+      $('#age-span').text(clacMercuryYears(age))
+      $('#expectancy-span').text(clacMercuryYears(getLifeExpectancy(selectedCountry)))
+    }
+  })
 });
