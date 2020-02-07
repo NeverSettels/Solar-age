d5476f3de6ef5f262efffd9ee06bab089005bdaf
NeverSettels <settels.eric@gmail.com>
2020-02-07 10:02:18 -0800

    second red  for checkif number function

--- __tests__/age.tests.js
+++ __tests__/age.tests.js
@@ -4,4 +4,8 @@ describe('checkIfnumber', () => {
   test('Should check that checkIfNumber function returns false if passed a string', () => {
     expect(checkIfNumber('hello')).toEqual(false);
   })
+
+  test('Should check that checkIfNumber function returns true if passed a number', () => {
+    expect(checkIfNumber(24)).toEqual(true);
+  })
 })
