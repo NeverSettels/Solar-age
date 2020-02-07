64586066d3f52d407558060c9cf37360b3cde0f2
NeverSettels <settels.eric@gmail.com>
2020-02-07 10:28:57 -0800

    first green  for calcVenusYears  function

--- src/age.logic.js
+++ src/age.logic.js
@@ -9,5 +9,5 @@ export function checkIfNumber(input) {
 export const clacMercuryYears = numYears => numYears * 0.24;
 
 export const calcVenusYears = numYears => {
-  return numYears;
+  return numYears * 0.62;
 }
