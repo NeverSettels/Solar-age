8beab4eecf1bc208ce0e83ece4ddf3389153ed9f
NeverSettels <settels.eric@gmail.com>
2020-02-07 15:05:41 -0800

    Collect info from form

--- src/main.js
+++ src/main.js
@@ -3,3 +3,16 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+
+console.log('pls')
+$(document).ready(() => {
+  // let selectedCountry = '';
+  // let age = 0;
+  $('#age-form').submit(event => {
+    event.preventDefault();
+    let selectedCountry = $("#country").children("option:selected").val();
+    let age = $('#age').val();
+    console.log(age, selectedCountry);
+  });
+});
