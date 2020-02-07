deb0a9ca47501b5d94e716f572247aa74b6c3c62
NeverSettels <settels.eric@gmail.com>
2020-02-07 10:36:10 -0800

    green for calcMarsYears  function

--- src/age.logic.js
+++ src/age.logic.js
@@ -7,4 +7,4 @@ export function checkIfNumber(input) {
 }
 export const clacMercuryYears = numYears => numYears * 0.24;
 export const calcVenusYears = numYears => numYears * 0.62;
-export const calcMarsYears = numYears => numYears;
+export const calcMarsYears = numYears => numYears * 1.88;
