79315edddba909a22ce3826321d8d557da666724
NeverSettels <settels.eric@gmail.com>
2020-02-07 13:39:21 -0800

    Imported necessary dependancies to main.js to build UI

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { checkIfNumber, clacMercuryYears, calcVenusYears, calcMarsYears, calcJupiterYears, getLifeExpectancy, leftToLive } from './age.logic.js';
+import $ from 'jquery';
+import './styles.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
