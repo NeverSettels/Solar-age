2cb14855b1ffc5f374d3f44e01cb0f1f60b69110
NeverSettels <settels.eric@gmail.com>
2020-02-07 16:41:49 -0800

    Clean up unecessary spaces delete console.logs and add missing ;

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-
-console.log('pls')
 $(document).ready(() => {
   let selectedCountry = '';
   let age = 0;
@@ -36,7 +34,6 @@ $(document).ready(() => {
       } else if (lefttoliveArr[1] === 'soon') {
         $('#left-to-live').text(`Careful you're at the avarage life expectancy as people from your country`);
       }
-
     } else if (planet === 'Mars') {
       $('#info').removeClass('no-display');
       $("#img").attr("src", "https://i.pinimg.com/originals/40/25/d8/4025d8936c88b454a95b857beac7eaec.png");
@@ -44,7 +41,6 @@ $(document).ready(() => {
       $('#fact').text('Mars is the fourth planet from the Sun and last of the terrestrial planets. Like the rest of the planets in the solar system (except Earth), Mars is named after a mythological figure - the Roman god of war. In addition to its official name, Mars is sometimes called the Red Planet because of the brownish-red color of its surface. Mars is the second smallest planet in the solar system behind Mercury.');
       $('#age-span').text(calcMarsYears(age));
       $('#expectancy-span').text(calcMarsYears(getLifeExpectancy(selectedCountry)));
-
       let lefttoliveArr = leftToLive(calcMarsYears(age), calcMarsYears(getLifeExpectancy(selectedCountry)));
       if (lefttoliveArr[1] === 'left') {
         $('#left-to-live').text(`You have on avarge ${lefttoliveArr[0]} years to live`);
@@ -60,7 +56,6 @@ $(document).ready(() => {
       $('#fact').text("Venus is the second planet from the Sun and the third brightest object in Earth's sky after the Sun and Moon. It is sometimes referred to as the sister planet to Earth, because their size and mass are so similar. Venus is also the closest planet to Earth. The surface of Venus is hidden by an opaque layer of clouds which are formed from sulphuric acid. The planet is named for Venus, the Roman goddess of love and beauty and is the second largest terrestrial planet.");
       $('#age-span').text(calcVenusYears(age));
       $('#expectancy-span').text(calcVenusYears(getLifeExpectancy(selectedCountry)));
-
       let lefttoliveArr = leftToLive(calcVenusYears(age), calcVenusYears(getLifeExpectancy(selectedCountry)));
       if (lefttoliveArr[1] === 'left') {
         $('#left-to-live').text(`You have on avarge ${lefttoliveArr[0]} years to live`);
@@ -76,7 +71,6 @@ $(document).ready(() => {
       $('#fact').text("Named after the Roman king of the gods, Jupiter is fitting of its name. With a mass of 1.90 x 1027 kg and a mean diameter of 139,822 km, Jupiter is easily the largest and most massive planet in the Solar System. To put this in perspective, it would take 11 Earths lined up next to each other to stretch from one side of Jupiter to the other and it would take 317 Earths to equal the mass of Jupiter.");
       $('#age-span').text(calcJupiterYears(age));
       $('#expectancy-span').text(calcJupiterYears(getLifeExpectancy(selectedCountry)));
-
       let lefttoliveArr = leftToLive(calcJupiterYears(age), calcJupiterYears(getLifeExpectancy(selectedCountry)));
       if (lefttoliveArr[1] === 'left') {
         $('#left-to-live').text(`You have on avarge ${lefttoliveArr[0]} years to live`);
@@ -87,13 +81,10 @@ $(document).ready(() => {
       }
     }
   });
-
   $('#back').click(event => {
     event.preventDefault();
     $('#info').addClass('no-display');
     $('.planets').removeClass('no-display');
     $('#click-on-planet').removeClass('no-display');
-
-
-  })
+  });
 });
