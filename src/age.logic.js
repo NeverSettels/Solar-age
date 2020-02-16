cb1979edbf6130f6cd003da752928b220a4cd2f7
NeverSettels <settels.eric@gmail.com>
2020-02-16 12:32:43 -0800

    Add class to age.logic and refactor correctly in main.js

--- src/age.logic.js
+++ src/age.logic.js
@@ -1,31 +1,33 @@
-export function checkIfNumber(input) {
-  if (typeof input === "string") {
-    return false;
-  } else {
-    return true;
+export class Planet {
+  checkIfNumber(input) {
+    if (typeof input === "string") {
+      return false;
+    } else {
+      return true;
+    }
   }
-}
-export const clacMercuryYears = numYears => numYears * 0.24;
-export const calcVenusYears = numYears => numYears * 0.62;
-export const calcMarsYears = numYears => numYears * 1.88;
-export const calcJupiterYears = numYears => numYears * 11.86;
+  clacMercuryYears = numYears => numYears * 0.24;
+  calcVenusYears = numYears => numYears * 0.62;
+  calcMarsYears = numYears => numYears * 1.88;
+  calcJupiterYears = numYears => numYears * 11.86;
 
-export function getLifeExpectancy(country) {
-  if (country === 'mexico') {
-    return 77;
-  } else if (country === 'usa') {
-    return 79;
-  } else if (country === 'canada') {
-    return 82;
+  getLifeExpectancy(country) {
+    if (country === 'mexico') {
+      return 77;
+    } else if (country === 'usa') {
+      return 79;
+    } else if (country === 'canada') {
+      return 82;
+    }
   }
-}
-export const leftToLive = (age, expectancy) => {
-  let yearsLeft = expectancy - age
-  if (yearsLeft > 0) {
-    return [yearsLeft, 'left'];
-  } else if (yearsLeft < 0) {
-    return [-1 * yearsLeft, 'past'];
-  } else if (yearsLeft === 0) {
-    return [yearsLeft, 'soon'];
+  leftToLive = (age, expectancy) => {
+    let yearsLeft = expectancy - age
+    if (yearsLeft > 0) {
+      return [yearsLeft, 'left'];
+    } else if (yearsLeft < 0) {
+      return [-1 * yearsLeft, 'past'];
+    } else if (yearsLeft === 0) {
+      return [yearsLeft, 'soon'];
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import { checkIfNumber, clacMercuryYears, calcVenusYears, calcMarsYears, calcJupiterYears, getLifeExpectancy, leftToLive } from './age.logic.js';
+import { Planet } from './age.logic.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(() => {
+  const myPlanet = new Planet;
   let selectedCountry = '';
   let age = 0;
   $('#age-form').submit(event => {
@@ -24,9 +25,9 @@ $(document).ready(() => {
       $("#img").attr("src", "https://nineplanets.org/wp-content/uploads/2019/09/mercury.png");
       $('#title').text(planet);
       $('#fact').text('Mercury is the closest planet to the Sun and is also the smallest of the eight planets in our solar system. For every 2 orbits of the Sun, which takes around 88 Earth days, Mercury completes three rotations of its axis. It is gravitationally locked and this rotation is unique to the solar system.');
-      $('#age-span').text(clacMercuryYears(age));
-      $('#expectancy-span').text(clacMercuryYears(getLifeExpectancy(selectedCountry)));
-      let lefttoliveArr = leftToLive(clacMercuryYears(age), clacMercuryYears(getLifeExpectancy(selectedCountry)));
+      $('#age-span').text(myPlanet.clacMercuryYears(age));
+      $('#expectancy-span').text(myPlanet.clacMercuryYears(myPlanet.getLifeExpectancy(selectedCountry)));
+      let lefttoliveArr = leftToLive(myPlanet.clacMercuryYears(age), myPlanet.clacMercuryYears(myPlanet.getLifeExpectancy(selectedCountry)));
       if (lefttoliveArr[1] === 'left') {
         $('#left-to-live').text(`You have on avarge ${lefttoliveArr[0]} years to live`);
       } else if (lefttoliveArr[1] === 'past') {
@@ -39,9 +40,9 @@ $(document).ready(() => {
       $("#img").attr("src", "https://i.pinimg.com/originals/40/25/d8/4025d8936c88b454a95b857beac7eaec.png");
       $('#title').text(planet);
       $('#fact').text('Mars is the fourth planet from the Sun and last of the terrestrial planets. Like the rest of the planets in the solar system (except Earth), Mars is named after a mythological figure - the Roman god of war. In addition to its official name, Mars is sometimes called the Red Planet because of the brownish-red color of its surface. Mars is the second smallest planet in the solar system behind Mercury.');
-      $('#age-span').text(calcMarsYears(age));
-      $('#expectancy-span').text(calcMarsYears(getLifeExpectancy(selectedCountry)));
-      let lefttoliveArr = leftToLive(calcMarsYears(age), calcMarsYears(getLifeExpectancy(selectedCountry)));
+      $('#age-span').text(myPlanet.calcMarsYears(age));
+      $('#expectancy-span').text(myPlanet.calcMarsYears(myPlanet.getLifeExpectancy(selectedCountry)));
+      let lefttoliveArr = leftToLive(myPlanet.calcMarsYears(age), myPlanet.calcMarsYears(myPlanet.getLifeExpectancy(selectedCountry)));
       if (lefttoliveArr[1] === 'left') {
         $('#left-to-live').text(`You have on avarge ${lefttoliveArr[0]} years to live`);
       } else if (lefttoliveArr[1] === 'past') {
@@ -54,9 +55,9 @@ $(document).ready(() => {
       $("#img").attr("src", "https://vignette.wikia.nocookie.net/thesolarsystem6361/images/6/6e/Venus_surface_spacepedia.png/revision/latest/scale-to-width-down/340?cb=20180322151813");
       $('#title').text(planet);
       $('#fact').text("Venus is the second planet from the Sun and the third brightest object in Earth's sky after the Sun and Moon. It is sometimes referred to as the sister planet to Earth, because their size and mass are so similar. Venus is also the closest planet to Earth. The surface of Venus is hidden by an opaque layer of clouds which are formed from sulphuric acid. The planet is named for Venus, the Roman goddess of love and beauty and is the second largest terrestrial planet.");
-      $('#age-span').text(calcVenusYears(age));
-      $('#expectancy-span').text(calcVenusYears(getLifeExpectancy(selectedCountry)));
-      let lefttoliveArr = leftToLive(calcVenusYears(age), calcVenusYears(getLifeExpectancy(selectedCountry)));
+      $('#age-span').text(myPlanet.calcVenusYears(age));
+      $('#expectancy-span').text(myPlanet.calcVenusYears(myPlanet.getLifeExpectancy(selectedCountry)));
+      let lefttoliveArr = leftToLive(myPlanet.calcVenusYears(age), myPlanet.calcVenusYears(myPlanet.getLifeExpectancy(selectedCountry)));
       if (lefttoliveArr[1] === 'left') {
         $('#left-to-live').text(`You have on avarge ${lefttoliveArr[0]} years to live`);
       } else if (lefttoliveArr[1] === 'past') {
@@ -69,9 +70,9 @@ $(document).ready(() => {
       $("#img").attr("src", "https://i2.wp.com/freepngimages.com/wp-content/uploads/2016/04/planet-jupiter-1.png?fit=800%2C800");
       $('#title').text(planet);
       $('#fact').text("Named after the Roman king of the gods, Jupiter is fitting of its name. With a mass of 1.90 x 1027 kg and a mean diameter of 139,822 km, Jupiter is easily the largest and most massive planet in the Solar System. To put this in perspective, it would take 11 Earths lined up next to each other to stretch from one side of Jupiter to the other and it would take 317 Earths to equal the mass of Jupiter.");
-      $('#age-span').text(calcJupiterYears(age));
-      $('#expectancy-span').text(calcJupiterYears(getLifeExpectancy(selectedCountry)));
-      let lefttoliveArr = leftToLive(calcJupiterYears(age), calcJupiterYears(getLifeExpectancy(selectedCountry)));
+      $('#age-span').text(myPlanet.calcJupiterYears(age));
+      $('#expectancy-span').text(myPlanet.calcJupiterYears(myPlanet.getLifeExpectancy(selectedCountry)));
+      let lefttoliveArr = leftToLive(myPlanet.calcJupiterYears(age), myPlanet.calcJupiterYears(myPlanet.getLifeExpectancy(selectedCountry)));
       if (lefttoliveArr[1] === 'left') {
         $('#left-to-live').text(`You have on avarge ${lefttoliveArr[0]} years to live`);
       } else if (lefttoliveArr[1] === 'past') {
